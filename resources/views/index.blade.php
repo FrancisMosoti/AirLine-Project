7bf054450d8d8fc9eee9307c0e44897161813597
GUYAHAVI <harveyelvis24@gmail.com>
2024-03-24 14:30:44 +0300

    added search flight form to homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -11,6 +11,64 @@
             <button class="btn btn-main">BOOK YOUR FLIGHT</button>
         </div>
     </div>
+    <div class="row search-flights">
+    <form action="{{ route('flight.search') }}" method="post">
+                @csrf
+
+               <div class="row">
+               <div class="col-md-3">
+                    <label style="color: rgb(252, 254, 254)" id="plane2">Departure date</label>
+                    <input type="date" name="date" id="plane2"
+                        class="form-control @error('date') is-invalid @enderror"
+                        value="{{ old('date') ? old('date') : '' }}">
+                    @error('date')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                
+                <div class="col-md-3">
+                    <label style="color: rgb(252, 254, 254)">Depart from:</label>
+                    <select id="town" name="depart" class="form-select @error('depart') is-invalid @enderror">
+                        <option value="" selected>Select Airport</option>
+                        <option value="Nairobi">Nairobi</option>
+                        <option value="Kisumu">Kisumu</option>
+                        <option value="Mombasa">Mombasa</option>
+                        <option value="Eldoret">Eldoret</option>
+                    </select>
+                    @error('depart')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+                
+                <div class="col-md-3">
+                    <label style="color: rgb(252, 254, 254)">Destination:</label>
+                    <select id="town" name="destination"
+                        class="form-select @error('destination') is-invalid @enderror">
+                        <option value="" selected>Select Airport</option>
+                        <option value="Nairobi">Nairobi</option>
+                        <option value="Kisumu">Kisumu</option>
+                        <option value="Mombasa">Mombasa</option>
+                        <option value="Eldoret">Eldoret</option>
+                    </select>
+                    @error('destination')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                </div>
+
+                <div class="col-md-3">
+                    <button type="submit" class="btn bg-white">Search-flight</button>
+                </div>
+               </div>
+            </form>
+      
+    </div>
     <div class="beautiful row">
         <div class="col-md-6 beautiful-image">
             <img src="https://images.pexels.com/photos/1268855/pexels-photo-1268855.jpeg?auto=compress&cs=tinysrgb&w=400">
@@ -22,6 +80,7 @@
             <button class="btn btn-main">EXPLORE NOW</button>
         </div>
     </div>
+    
     <div class="row hotels">
         <div class="col-md-6 ">
             <div class="hotels-txt">
